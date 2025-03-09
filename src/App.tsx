03ee9f5c62c0ed30edbb34e20f3fca516845e500
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-03-09 14:26:28 +0600

    dashboard sidbar issue fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-
 import './App.css'
+import ProductsPage from './pages/ProductsPage';
+import { Route, Routes } from 'react-router';
+import OverviewPage from './pages/OverviewPage';
+import Sidebar from './components/common/Sidebar';
 
 function App() {
 
   return (
-   <>
-   <h1 className='text-red-700'>Hello world</h1>
-   </>
+    <div className='flex h-screen bg-gray-900 text-gray-100 overflow-hidden'>
+    {/* BG */}
+    <div className='fixed inset-0 z-0'>
+      <div className='absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80' />
+      <div className='absolute inset-0 backdrop-blur-sm' />
+    </div>
+
+    <Sidebar />
+    <Routes>
+      <Route path='/' element={<OverviewPage />} />
+      <Route path='/products' element={<ProductsPage />} />
+      {/* <Route path='/users' element={<UsersPage />} />
+      <Route path='/sales' element={<SalesPage />} />
+      <Route path='/orders' element={<OrdersPage />} />
+      <Route path='/analytics' element={<AnalyticsPage />} />
+      <Route path='/settings' element={<SettingsPage />} /> */}
+    </Routes>
+  </div>
   )
 }
 
