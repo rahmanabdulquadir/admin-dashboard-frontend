f8d789574b9eb9d86aa1b569a9cb13227153cc73
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-03-10 15:43:10 +0600

    settings page completed

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,6 +1,9 @@
 import Header from "../components/common/Header";
 import ConnectedAccounts from "../components/settings/ConnectedAccounts";
+import DangerZone from "../components/settings/DangerZone";
+import Notifications from "../components/settings/Notifications";
 import Profile from "../components/settings/Profile";
+import Security from "../components/settings/Security";
 
 
 const SettingsPage = () => {
@@ -9,9 +12,9 @@ const SettingsPage = () => {
 			<Header title='Settings' />
 			<main className='max-w-4xl mx-auto py-6 px-4 lg:px-8'>
 				<Profile />
-				{/* <Notifications />
+				<Notifications />
 				<Security />
-				<DangerZone /> */}
+				<DangerZone />
 				<ConnectedAccounts />
 			</main>
 		</div>
